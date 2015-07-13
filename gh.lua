205536672f0879a0c61f9953124dbf719922c78f
David O'Trakoun <dotrakoun@timeinc.com>
2015-07-13 18:25:38 -0400

    add gh script

---
+++ gh.lua
@@ -0,0 +1,48 @@
+--
+-- github repo client
+-- v1.0.0
+-- pastebin QwW6Xg6M
+-- by @davidosomething
+--
+-- based on https://raw.githubusercontent.com/seriallos/computercraft/master/gist.lua
+--
+
+local USERNAME = "davidosomething"
+
+if not http then
+  print("GitHub requires HTTP API")
+  return
+end
+
+local tArgs = { ... }
+
+if (#tArgs ~= 3) then
+  print( "USAGE: dko get FILEPATH DEST" )
+  return
+end
+
+local action = tArgs[1]
+local filepath = tArgs[2]
+local program = tArgs[3]
+
+if "get" ~= action then
+  print( "Only 'get' is supported right now" )
+  return
+end
+
+if fs.exists( program ) then
+  print( "File "..program.." already exists.  No action taken" )
+  return
+end
+
+-- TODO: maybe handle multifile gists?
+local url = "https://raw.githubusercontent.com/" .. USERNAME .. "/computercraft/master/" .. filepath
+
+local request = http.get( url )
+local response = request.readAll()
+request.close()
+
+local file = fs.open( program, "w" )
+file.write( response )
+file.close()
+
