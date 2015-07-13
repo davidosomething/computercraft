b7aa152cbcf87b34958dd99fe96b80486bfb7982
David O'Trakoun <dotrakoun@timeinc.com>
2015-07-13 17:36:53 -0400

    reactor and remote status display and key-handling

--- reactor/main.lua
+++ reactor/main.lua
@@ -1,9 +1,21 @@
 --
 -- reactor/main
--- v2.1.5
+-- v2.2.0
 -- pastebin 710inmxN
 -- by @davidosomething
 --
+
+
+-- -----------------------------------------------------------------------------
+-- Program state ---------------------------------------------------------------
+-- -----------------------------------------------------------------------------
+autotoggle = true
+exit = false
+
+
+-- -----------------------------------------------------------------------------
+-- Peripheral config -----------------------------------------------------------
+-- -----------------------------------------------------------------------------
 
 -- monitor
 local m = peripheral.find('monitor')
@@ -12,16 +24,25 @@ if m then
 end
 
 -- reactor
-local r = peripheral.wrap('back')
+local reactorSide = 'back'
+if peripheral.isPresent(reactorSide) then
+  local r = peripheral.wrap('back')
+else
+  exit = true
+end
 
 -- modem
 local modemSide = 'left'
 local w = peripheral.wrap(modemSide)
 rednet.open(modemSide)
 
-autotoggle = true
-exit = false
 
+-- -----------------------------------------------------------------------------
+-- Functions -------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
+
+-- autotoggle
+--
 local function autotoggle()
   -- no fuel, leave off
   if r.getFuelAmount() == 0 then
@@ -42,6 +63,17 @@ local function autotoggle()
   end
 end
 
+
+-- status_label
+--
+local function status_label(text)
+  m.setTextColor(colors.white)
+  term.write(text)
+end
+
+
+-- status
+--
 local function status()
   if r.getActive() then
     m.setTextColor(colors.lime)
@@ -55,45 +87,81 @@ local function status()
 
   print('energy: ' .. r.getEnergyStored() .. '/10000000 RF')
   print('output: ' .. r.getEnergyProducedLastTick() .. ' RF/t')
-  print('fuel:   ' .. r.getFuelAmount() .. '/' .. r.getFuelAmountMax() .. ' mb')
-  print('usage:  ' .. r.getFuelConsumedLastTick() .. ' mb/t')
-  print('waste:  ' .. r.getWasteAmount() .. ' mb')
-  print('core:   ' .. r.getFuelTemperature() .. ' C')
-  print('case:   ' .. r.getCasingTemperature() .. ' C')
+
+  status_label('fuel:   ')
+  m.setTextColor(colors.yellow)
+  term.write(r.getFuelAmount())
+  m.setTextColor(colors.lightGray)
+  term.write('/' .. r.getFuelAmountMax() .. 'mb\n')
+
+  status_label('waste:  ')
+  m.setTextColor(colors.lightBlue)
+  term.write(r.getWasteAmount())
+  m.setTextColor(colors.lightGray)
+  term.write('mb')
+
+  print('usage:  ' .. r.getFuelConsumedLastTick() .. 'mb/t')
+  print('core:   ' .. r.getFuelTemperature() .. 'C')
+  print('case:   ' .. r.getCasingTemperature() .. 'C')
 
   if autotoggle then
     print('autotoggle: ON' )
   else
     print('autotoggle: OFF' )
   end
+
+  print()
+  print("q)uit  t)oggle  a)utotoggle")
+  print()
 end
+
 
 -- toggle_reactor
 --
 -- Switch reactor on/off
 --
-local function toggle_reactor()
-  if r.getActive() then
-    r.setActive(false)
-  else
-    r.setActive(true)
+-- nil,boolean state - toggle if nil, on if true, off if false
+local function toggle_reactor(state)
+  -- toggle
+  if state == nil then
+    state = not r.getActive()
   end
+
+  -- set to exact
+  r.setActive(state)
 end
 
+
+-- get_monitor_touch
+--
 local function get_monitor_touch()
   local event, side, x, y = os.pullEvent('monitor_touch')
   toggle_reactor()
 end
 
+
+-- get_timeout
+--
 local function get_timeout()
   local event, timerHandler = os.pullEvent('timer')
-  autotoggle()
+  if autotoggle then
+    autotoggle()
+  end
 end
 
+
+-- get_modem_message
+--
 local function get_modem_message()
   local senderId, message, protocol = rednet.receive('reactor')
-  if message == 'toggle' then
+  if message == 'autotoggle' then
     toggle_reactor()
+  elseif message == 'toggle' then
+    toggle_reactor()
+  elseif message == 'on' then
+    toggle_reactor(true)
+  elseif message == 'off' then
+    toggle_reactor(false)
   end
 
   local message = ''
@@ -106,15 +174,34 @@ local function get_modem_message()
 
   message = message .. '\nrf: ' .. r.getEnergyStored()
   message = message .. '\nfuel: ' .. r.getFuelAmount() .. '/' .. r.getFuelAmountMax()
+
+  if autotoggle then
+    message = message .. '\nauto: ON'
+  else
+    message = message .. '\nauto: OFF'
+  end
+
   rednet.send(senderId, message, 'remote')
 end
 
+
+-- get_key
+--
 local function get_key()
-  local event, code = os.pullEvent('key_up')
-  if code == key.q then
+  local event, code = os.pullEvent('key')
+  if code == key.a then
+    autotoggle = not autotoggle
+  elseif code == key.t then
+    toggle_reactor()
+  elseif code == key.q then
     exit = true
   end
 end
+
+
+-- -----------------------------------------------------------------------------
+-- Main ------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
 
 while not exit do
   local myTimer = os.startTimer(1)
@@ -126,5 +213,4 @@ while not exit do
   parallel.waitForAny(get_key, get_monitor_touch, get_modem_message, get_timeout)
   os.cancelTimer(myTimer)
 end
-
 
--- remote/main.lua
+++ remote/main.lua
@@ -1,9 +1,20 @@
 --
 -- remote/main
--- v2.1.8
+-- v2.2.0
 -- pastebin SHyMGSSK
 -- by @davidosomething
 --
+
+
+-- -----------------------------------------------------------------------------
+-- Program state ---------------------------------------------------------------
+-- -----------------------------------------------------------------------------
+exit = false
+
+
+-- -----------------------------------------------------------------------------
+-- Peripheral config -----------------------------------------------------------
+-- -----------------------------------------------------------------------------
 
 -- remote reactor
 local reactorPort = 1
@@ -13,39 +24,73 @@ local modemSide = 'back'
 local w = peripheral.wrap(modemSide)
 rednet.open(modemSide)
 
-local function do_action(action)
-  if action == 'toggle' then
-    rednet.send(reactorPort, 'toggle', 'reactor')
-  else
-    rednet.send(reactorPort, 'status', 'reactor')
-  end
 
-  local senderId, message, protocol = rednet.receive('remote')
-  print(message)
-  print()
-  print()
+-- -----------------------------------------------------------------------------
+-- Functions -------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
+
+-- do_action
+--
+local function do_action(action)
+  if action == 'autotoggle' then
+    rednet.send(reactorPort, 'autotoggle', 'reactor')
+  elseif action == 'toggle' then
+    rednet.send(reactorPort, 'toggle', 'reactor')
+  end
 end
 
+-- get_status
+--
+-- Request status messages from reactors over rednet and display
+--
+local function get_status()
+  rednet.send(reactorPort, 'status', 'reactor')
+  local senderId, message, protocol = rednet.receive('remote')
+  print(message)
+end
+
+
+-- get_key
+--
+local function get_key()
+  local event, code = os.pullEvent('key')
+  if code == keys.a then
+    do_action('autotoggle')
+  elseif code == keys.t then
+    do_action('toggle')
+  elseif code == keys.q then
+    exit = true
+  end
+end
+
+
+-- get_timeout
+--
 local function get_timeout()
   local event, timerHandler = os.pullEvent('timer')
 end
 
+
+-- usage
+--
 local function usage()
-  print("q)uit  t)oggle  s)tatus")
+  print("q)uit  t)oggle  a)utotoggle")
   print()
 end
 
-exit = false
-while not exit do
-  usage()
 
-  local event, code = os.pullEvent('key')
-  if code == keys.t then
-    do_action('toggle')
-  elseif code == keys.s then
-    do_action('status')
-  elseif code == keys.q then
-    exit = true
-  end
+-- -----------------------------------------------------------------------------
+-- Main ------------------------------------------------------------------------
+-- -----------------------------------------------------------------------------
+
+while not exit do
+  local myTimer = os.startTimer(1)
+  m.clear()
+  m.setCursorPos(1,1)
+  usage()
+  get_status()
+
+  parallel.waitForAny(get_key, get_timeout)
+  os.cancelTimer(myTimer)
 end
 
